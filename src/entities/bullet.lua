22d0a6d81b111e1d03ad45feb22ab5f1d4571a7c
phortuin <phortuin@users.noreply.github.com>
2025-04-10 11:40:41 +0200

    Create bullet module
    - Bullets have a shadow just like ships. They take the ship’s
      velocity and multiply it by 8. The bullet moves faster or
      slower depending on the ship’s velocity as it supposedly
      is in nature, too
    - Bullets bounce off the edges of the screen for a number of
      times before they are removed

---
+++ src/entities/bullet.lua
@@ -0,0 +1,83 @@
+local Vector = require "src.lib.vector"
+local drawable = require "src.util.drawable"
+local util = require "src.util.util"
+
+---@class Bullet
+---@field position Vector
+---@field direction Vector
+---@field velocity Vector
+---@field sprite unknown Love sprite
+---@field shadow unknown Love sprite
+---@field hitEdge number Counter for how many times the bullet has hit the edge of the screen
+local Bullet = {}
+Bullet.__index = Bullet
+
+local BULLET_SPEED = 8;
+
+-- Create a new bullet
+---@param position Vector
+---@param direction Vector
+---@param velocity Vector
+---@return Bullet
+function Bullet:new(position, direction, velocity)
+	local bullet = {
+		position = position,
+		direction = direction,
+		velocity = velocity,
+		sprite = love.graphics.newImage("assets/sprites/bullet.png"),
+		shadow = love.graphics.newImage("assets/sprites/bullet-shadow.png"),
+		hitEdgeCounter = 0
+	}
+
+	local f = Vector:new(bullet.direction.x, bullet.direction.y)
+	f:mult(BULLET_SPEED)
+	bullet.velocity:add(f)
+
+	setmetatable(bullet, Bullet)
+	return bullet
+end
+
+-- Update the bullet's position
+---@return nil
+function Bullet:update()
+	self.position:add(self.velocity)
+	self:bounceEdges()
+end
+
+-- Draw the bullet
+---@return nil
+function Bullet:draw()
+	drawable.draw(self, 1, false)
+end
+
+-- Draw the bullet's shadow
+---@return nil
+function Bullet:drawShadow()
+	drawable.drawShadow(self)
+end
+
+-- Check if the bullet has hit the edge of the screen. Make sure it stays
+-- within bounds, and invert velocity when it hits the edge. A counter is used
+-- to track how many times the bullet has hit the edge.
+---@return nil
+function Bullet:bounceEdges()
+	local xMax = love.graphics.getWidth()
+	local yMax = love.graphics.getHeight()
+
+	-- Bounce horizontally
+	if self.position.x > xMax or self.position.x < 0 then
+		self.velocity.x = self.velocity.x * -1
+		self.hitEdgeCounter = self.hitEdgeCounter + 1
+	end
+
+	-- Bounce vertically
+	if self.position.y > yMax or self.position.y < 0 then
+		self.velocity.y = self.velocity.y * -1
+		self.hitEdgeCounter = self.hitEdgeCounter + 1
+	end
+
+	self.position.x = util.clamp(self.position.x, 0, xMax)
+	self.position.y = util.clamp(self.position.y, 0, yMax)
+end
+
+return Bullet
